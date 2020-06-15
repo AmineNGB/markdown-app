1d68b9cee84887972f4cfaf6af03a4193fd207b9
Amine <amineneghbel@gmail.com>
2020-06-15 16:01:55 +0200

    markdown features with marked done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import marked from 'marked';
 
-export default App;
+import { markedText } from './markedText';
+
+export default class App extends Component {
+  state = {
+    text: markedText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              onChange={this.handleChange}
+              value={ this.state.text }
+              classname='form-control'
+              rows="35" />
+          </div>
+          <div className="col-sm-6">
+            <div 
+            dangerouslySetInnerHTML={this.renderText
+            (this.state.text)} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
